82cb4462cd2d4547d2432abdbec7d291bd20b7ff
Shailth13 <97862443+Shailth13@users.noreply.github.com>
2022-07-30 22:51:20 -0500

    change password update

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -2,36 +2,40 @@ import React from 'react'
 import { Navbar } from './navbar'
 import { Link } from 'react-router-dom'
 import './ChangePassword.css'
+import Footer from './Footer/Footer'
 
 export const ChangePassword = () => {
   return (
     <>
       <Navbar />
-      <section id='chgnpassword-page'>
-        <form classNameName='chgnpassword-form' action='authenticate.php' method='post'>
-          <h3>Change Password</h3>
-          <div classNameName='form-body'>
-            <div classNameName='column'>
-              <div classNameName='input-group'>
-                <label>Old Password </label>
-                <input type='password' name='oldpwd' placeholder='Enter your password' required />
-              </div>
-              <div classNameName='input-group'>
-                <label>New Password </label>
-                <input type='password' name='password' placeholder='Enter your password' required />
-              </div>
-              <div classNameName='input-group'>
-                <label>Confirm Password </label>
-                <input type='password' name='confpwd' placeholder='Enter your password' required />
+      <div className='bodyWrapper'>
+        <section id='chgnpassword-page'>
+          <form className='chgnpassword-form' action='authenticate.php' method='post'>
+            <h3>Change Password</h3>
+            <div className='form-body'>
+              <div className='column'>
+                <div className='input-group'>
+                  <label>Old Password </label>
+                  <input type='password' name='oldpwd' placeholder='Enter your password' required />
+                </div>
+                <div className='input-group'>
+                  <label>New Password </label>
+                  <input type='password' name='password' placeholder='Enter your password' required />
+                </div>
+                <div className='input-group'>
+                  <label>Confirm Password </label>
+                  <input type='password' name='confpwd' placeholder='Enter your password' required />
+                </div>
               </div>
             </div>
-          </div>
-          <div classNameName='form-footer'>
-            <input classNameName='btn btn_primary' type='submit' value='Update' />
-            <Link to='/dashboard'>Back to dashboard</Link>
-          </div>
-        </form>
-      </section>
+            <div className='form-footer'>
+              <input className='btn btn_primary' type='submit' value='Update' />
+              <Link to='/dashboard'>Back to dashboard</Link>
+            </div>
+          </form>
+        </section>
+      </div>
+      <Footer />
     </>
   )
 }
